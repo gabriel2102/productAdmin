a93f503d365034a2a947891de055d8d4ccc6f3f9
Gabriel <gabrielespm@gmail.com>
2022-04-09 17:26:03 -0400

    segundo borrado

--- server/config/mongoose.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/product-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>console.log('We are connecting with database'))
-.catch((err)=>console.log(err));
--- server/routes/product.route.js
+++
@@ -1,5 +0,0 @@
-const ProductController = require('../controllers/product.controller');
-
-module.exports = app => {
-    app.post('/api/product/new', ProductController.createProduct);
-}
